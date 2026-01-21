0e9e8eb1e606b021db117cecd105bfc03025b602
Yxxsung <sophia.alexander@aol.com>
2026-01-21 09:40:09 -0500

    Fourth Commit of Assignment 'Simple Shapes'

--- SimpleShapes V1.0.py
+++ SimpleShapes V1.0.py
@@ -2,9 +2,17 @@
 #Class: CGT 115 - Spring 2026
 #Sophia Alexander      01/21/26
 
+#For some reason, it will not let me change the branch to main. I think this is what is causing
+#the commit and push issue, so I will update it to master and delete main like I did in the
+#previous repository.
+
 import Turtle
 import time
 
 def TriangleSide ():
     my_turtle.forward(100)
     my_turtle.right(45)
+
+def SquareSide ():
+    my_turtle.forward(100)
+    my_turtle.right(90)
